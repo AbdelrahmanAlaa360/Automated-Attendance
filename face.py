78fdc589a78a8cd8e468f23fdfdbabfa13df132d
AbdelrahmanAlaa360 <abdoalaasadik@gmail.com>
2022-11-08 16:37:22 +0200

    Train on multiple images

--- face.py
+++ face.py
@@ -1,10 +1,13 @@
+from re import sub
 import cv2
 import face_recognition
 import os
 from simple_facerec import SimpleFacerec
 
 sfr = SimpleFacerec()
-sfr.load_encoding_images("Face Recognition/images/")
+subfolders = [ f.path for f in os.scandir("Face Recognition/images/") if f.is_dir() ]
+for folder in subfolders:
+    sfr.load_encoding_images(folder)
 
 cap = cv2.VideoCapture(1)
 names = set()
@@ -14,7 +17,7 @@ while True:
     for face_loc, name in zip(face_locations, face_names):
         y1, x2, y2, x1 = face_loc[0], face_loc[1], face_loc[2], face_loc[3]
         cv2.putText(frame, name,(x1, y1 - 10), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 200, 0), 2)
-        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 200, 0), 2)
+        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 200, 0), 3)
         names.add(name)
 
     cv2.imshow('Frame', frame)
--- simple_facerec.py
+++ simple_facerec.py
@@ -1,3 +1,4 @@
+from email.mime import base
 import face_recognition
 import cv2
 import os
@@ -29,15 +30,17 @@ class SimpleFacerec:
             rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
             # Get the filename only from the initial file path.
-            basename = os.path.basename(img_path)
-            (filename, ext) = os.path.splitext(basename)
+            basename = os.path.dirname(img_path)
+            # Get folder name
+            folder_name = os.path.basename(basename)
+            # (filename, ext) = os.path.splitext(basename)
             # Get encoding
             img_encoding = face_recognition.face_encodings(rgb_img)[0]
 
             # Store file name and file encoding
             self.known_face_encodings.append(img_encoding)
-            self.known_face_names.append(filename)
-        print("Encoding images loaded")
+            self.known_face_names.append(folder_name)
+        print(folder_name, "Encoding images loaded")
 
     def detect_known_faces(self, frame):
         small_frame = cv2.resize(frame, (0, 0), fx=self.frame_resizing, fy=self.frame_resizing)
